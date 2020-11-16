098af2a860dc96835e875fd67a1d3abc1a4ec58f
cjchao <cjchao@sogaa.net>
2020-11-16 17:59:57 +0800

    first commit

---
+++ ApiTest/ApiTest/urls.py
@@ -0,0 +1,39 @@
+"""ApiTest URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path
+from MyApp.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('welcome/', welcome),											#获取菜单	
+    path('home/', home),												#首页
+    re_path(r"^child/(?P<eid>.+)/(?P<oid>.*)/$", child),				#返回子页面
+    path('login/', login),												#登录界面
+    path('login_action/', login_action),								#登录
+    path('register_action/', register_action),							#注册
+    path('accounts/login/', login),										#非登录状态下自动跳回登录界面
+    path('logout/', logout),											#退出
+    path('pei/', pei),													#匿名吐槽
+    path('help/', api_help),											#帮助
+    path('project_list/', project_list),								#项目列表
+    path('add_project/', add_project),									#增加项目
+    path('delect_project/', delect_project),							#删除项目
+    re_path(r"apis/(?P<id>.*)/$", open_apis),							#进入项目
+    re_path(r"^cases/(?P<id>.*)/$", open_cases),						#进入用例设置
+    re_path(r"^project_set/(?P<id>.*)/$", open_project_set),			#进入项目设置
+    re_path(r"^save_project_set/(?P<id>.*)/$", save_project_set),		#进入项目设置
+]
---
+++ ApiTest/MyApp/views.py
@@ -0,0 +1,144 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from MyApp.models import *
+
+
+
+@login_required
+def welcome(request):
+	return render(request, 'welcome.html')
+
+#控制不同的页面返回不同的数据：数据分发器
+def child_json(eid, oid=''):
+	res = {}
+	if eid == 'home.html':
+		data = DB_home_href.objects.all()
+		res = {"hrefs": data}
+	if eid == 'project_list.html':
+		data = DB_project.objects.all()
+		res = {"projects": data}
+
+	if eid == 'P_apis.html':
+		project = DB_project.objects.filter(id=oid)[0]
+		res = {"project": project}
+
+	if eid == 'P_cases.html':
+		project = DB_project.objects.filter(id=oid)[0]
+		res = {"project": project}
+
+	if eid == 'P_project_set.html':
+		project = DB_project.objects.filter(id=oid)[0]
+		res = {"project": project}
+
+	return res
+
+#返回子页面
+def child(request, eid, oid):
+	res = child_json(eid, oid)
+	return render(request, eid, res)
+
+
+
+def login(request):
+	return render(request, 'login.html')
+
+#登录
+def login_action(request):
+	u_name = request.GET['username']
+	p_word = request.GET['password']
+
+	#连通django用户表验证用户名、密码
+	from django.contrib import auth
+
+	user = auth.authenticate(username=u_name, password=p_word)
+	if user is not None:
+		#正确时
+		# return HttpResponseRedirect('/home/')
+		auth.login(request, user)
+		request.session['user'] = u_name
+		return HttpResponse('成功')
+	else:
+		#错误时
+		return HttpResponse('失败')
+
+#注册
+def register_action(request):
+	u_name = request.GET['username']
+	p_word = request.GET['password']
+
+	#连通django用户表，验证用户名、密码
+	from django.contrib.auth.models import User
+
+	try:
+		user = User.objects.create_user(username=u_name, password=p_word)
+		user.save()
+		return HttpResponse('注册成功')
+	except:
+		return HttpResponse('注册失败~用户名已存在')
+
+#首页
+@login_required
+def home(request):
+    return render(request,'welcome.html',{"whichHTML": "home.html","oid": ""})
+
+#退出
+def logout(request):
+	from django.contrib import auth
+	auth.logout(request)
+	return HttpResponseRedirect('/login/')
+
+#吐槽
+def pei(request):
+	tucao_text = request.GET['tucao_text']
+	#写	入数据库
+	DB_tucao.objects.create(user=request.user.username, text=tucao_text)
+	return HttpResponse('')
+
+#帮助
+def api_help(request):
+	return render(request, 'welcome.html', {"whichHTML": "help.html", "oid": ""})
+
+#项目列表
+def project_list(request):
+	return render(request, 'welcome.html', {"whichHTML": "project_list.html", "oid": ""})
+
+#新增项目
+def add_project(request):
+	project_name = request.GET['project_name']
+	DB_project.objects.create(name=project_name,remark='', user=request.user.username, other_user='')
+	return HttpResponse('')
+
+#删除项目
+def delect_project(request):
+	id = request.GET['id']
+	DB_project.objects.filter(id=id).delete()
+	return HttpResponse('')
+
+#进入接口库
+def open_apis(request, id):
+	project_id = id
+	return render(request, 'welcome.html', {"whichHTML": "P_apis.html", "oid": project_id})
+
+
+#进入用例设置
+def open_cases(request, id):
+	project_id = id
+	return render(request, 'welcome.html', {"whichHTML": "P_cases.html", "oid": project_id})
+
+
+#进入项目设置	
+def open_project_set(request, id):
+	project_id = id
+	return render(request, 'welcome.html', {"whichHTML": "P_project_set.html", "oid": project_id})
+
+
+#保存项目设置	
+def save_project_set(requset,id):
+    project_id = id
+    name = requset.GET['name']
+    remark = requset.GET['remark']
+    other_user = requset.GET['other_user']
+    DB_project.objects.filter(id=project_id).update(name=name,remark=remark,other_user=other_user)
+    return HttpResponse('')
+
